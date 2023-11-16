68ceb4249c3bf34cb7ddacc1d19edac839281d70
Jay Termulo <jetermulo@gmail.com>
2023-11-17 02:53:32 +0800

    build: hero section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,14 +4,16 @@ import Navbar, { NavList } from "../navbar/Navbar";
 
 function Header() {
     return (
-        <header className="navbar bg-base-100 p-0 justify-between">
-            <div className="navbar-start btn btn-ghost flex text-xl p-0 w-60 sm:w-96 flex justify-start mb-2 items-center">
-                <img src={logo} alt="logo" width={50} />
-                <h5 className="text-lg md:text-2xl">Bulakenya Gift Shop</h5>
-            </div>
-            <div className="hidden sm:flex">{NavList}</div>
-            <div className="flex sm:hidden">
-                <Navbar />
+        <header>
+            <div className="navbar bg-base-100 p-0 justify-between fixed top-0 w-full max-w-7xl z-10">
+                <div className="navbar-start btn btn-ghost text-xl p-0 w-60 sm:w-96 flex justify-start mb-2 items-center">
+                    <img src={logo} alt="logo" width={50} />
+                    <h5 className="text-lg md:text-2xl">Bulakenya Gift Shop</h5>
+                </div>
+                <nav className="hidden md:flex">{NavList}</nav>
+                <nav className="flex md:hidden">
+                    <Navbar />
+                </nav>
             </div>
         </header>
     );
