89e254aa8e3b5135b5972588091feb073728c44e
jayelemar <jetermulo@gmail.com>
2023-11-22 02:22:52 +0800

    Revert "feat: react query and axios for images"

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,15 @@ import store from "./utils/redux/store.tsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ModalContextProvider } from "./utils/context/modalContext.tsx";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Provider store={store}>
-                <BrowserRouter>
-                    <ModalContextProvider>
-                        <App />
-                    </ModalContextProvider>
-                </BrowserRouter>
-            </Provider>
-        </QueryClientProvider>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ModalContextProvider>
+                    <App />
+                </ModalContextProvider>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
