41c3970438c6dc0cf2d3fab6e665a0697b35b667
Jay Termulo <jetermulo@gmail.com>
2023-11-22 02:40:55 +0800

    revert:react query and axios

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,20 @@ import store from "./utils/redux/store.tsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ModalContextProvider } from "./utils/context/modalContext.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <ModalContextProvider>
-                    <App />
-                </ModalContextProvider>
-            </BrowserRouter>
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ModalContextProvider>
+                        <App />
+                    </ModalContextProvider>
+                </BrowserRouter>
+            </Provider>
+        </QueryClientProvider>
     </React.StrictMode>
 );
