1a2e39618ee2a5a06a0fa40e4cf59737c7ec7233
Jay Termulo <jetermulo@gmail.com>
2023-11-23 08:43:45 +0800

    feat: stat

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import Hero from "../../components/hero/Hero";
 import About from "../about/About";
+<<<<<<< Updated upstream
+=======
+
+import { Hero, Stat } from "../../components";
+import Contact from "../contact/Contact";
+>>>>>>> Stashed changes
 
 function Home() {
     return (
         <main>
             <Hero />
+<<<<<<< Updated upstream
             <About />
+=======
+            <Stat />
+            <About />
+
+            <Contact />
+>>>>>>> Stashed changes
         </main>
     );
 }
